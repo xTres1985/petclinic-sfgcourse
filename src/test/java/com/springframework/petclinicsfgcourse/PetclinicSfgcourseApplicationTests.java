d0436fa5a1d5f1e07498783133514b0abd363e45
Borozynski <boguslaw.borozynski@atos.net>
2021-03-10 12:00:55 +0100

    initial project add

---
+++ src/test/java/com/springframework/petclinicsfgcourse/PetclinicSfgcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.petclinicsfgcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicSfgcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
