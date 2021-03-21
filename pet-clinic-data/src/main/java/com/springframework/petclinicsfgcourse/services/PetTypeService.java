7d4a940d139191bc6a847281a4d0d2abd3a55290
xTres1985 <boguslaw.borozynski@gmail.com>
2021-03-21 22:09:05 +0100

    PetType mapservice implemented. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinicsfgcourse/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.petclinicsfgcourse.services;
+
+import com.springframework.petclinicsfgcourse.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
