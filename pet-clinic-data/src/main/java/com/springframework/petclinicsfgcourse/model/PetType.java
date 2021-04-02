9a5d8788e981e2b8fd6638518274e0f64f568f28
xTres1985 <boguslaw.borozynski@gmail.com>
2021-04-02 15:03:20 +0200

    refactored to add lombok. closes #51

--- pet-clinic-data/src/main/java/com/springframework/petclinicsfgcourse/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinicsfgcourse/model/PetType.java
@@ -1,21 +1,21 @@
 package com.springframework.petclinicsfgcourse.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
-@Table(name ="types")
-public class PetType extends BaseEntity{
+@Table(name = "types")
+public class PetType extends BaseEntity {
 
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
